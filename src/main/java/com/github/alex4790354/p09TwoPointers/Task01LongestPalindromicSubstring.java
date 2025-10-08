52aa6584aa6e4856ec5edd676cce8054c69e89ca
Alex4790354 <9720560@gmail.com>
2025-10-08 20:39:41 +0300

    + longest palindrom

--- src/main/java/com/github/alex4790354/p09TwoPointers/Task01LongestPalindromicSubstring.java
+++ src/main/java/com/github/alex4790354/p09TwoPointers/Task01LongestPalindromicSubstring.java
@@ -1,12 +1,11 @@
 package com.github.alex4790354.p09TwoPointers;
 
 /** Given a string s, return the longest palindromic substring in s.
- *
  * Example 1:
  * Input: s = "babad"
  * Output: "bab"
  * Explanation: "aba" is also a valid answer.
- *
+
  * Example 2:
  * Input: s = "cbbd"
  * Output: "bb"
